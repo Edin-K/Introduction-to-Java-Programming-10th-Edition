ac550351f527a556cc21513fd38b07c8272424ce
Edin-K <edin.korkic@hotmail.com>
2018-03-04 20:59:55 +0100

    First commit

--- src/programmingexercises/chapter2/Exercises_12.java
+++ src/programmingexercises/chapter2/Exercises_12.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * take-off speed v, you can compute the minimum runway length needed for an
  * airplane to take off using the following formula: length = v^2 /2a 
  * Write a program that prompts the user to enter v in meters/second (m/s) and 
- * the acceleration a in meters/second squared (m/s2), and displays the minimum
+ * the acceleration a in meters/second squared (m/s^2), and displays the minimum
  * runway length.
  * 
  * @uthor Edin Korkic
